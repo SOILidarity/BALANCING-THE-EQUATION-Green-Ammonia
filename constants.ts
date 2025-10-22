8ec9628fcc61dac20001518442638db51ef1ae87
SOILidarity <chaseflores70@gmail.com>
2025-10-22 16:17:29 +0800

    Delete constants.ts

--- constants.ts
+++
@@ -1,9 +0,0 @@
-
-export const NAV_LINKS = [
-  { href: '#problem', label: 'The Problem' },
-  { href: '#solution', label: 'The Solution' },
-  { href: '#benefits', label: 'Benefits' },
-  { href: '#calculator', label: 'Calculator' },
-  { href: '#petition', label: 'Petition' },
-  { href: '#team', label: 'The Team' },
-];
