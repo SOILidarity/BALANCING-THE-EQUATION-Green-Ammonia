a878af5de7f43748bc312258f21bd90a84cbc9da
SOILidarity <chaseflores70@gmail.com>
2025-10-22 15:59:32 +0800

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,9 @@
+
+export const NAV_LINKS = [
+  { href: '#problem', label: 'The Problem' },
+  { href: '#solution', label: 'The Solution' },
+  { href: '#benefits', label: 'Benefits' },
+  { href: '#calculator', label: 'Calculator' },
+  { href: '#petition', label: 'Petition' },
+  { href: '#team', label: 'The Team' },
+];
